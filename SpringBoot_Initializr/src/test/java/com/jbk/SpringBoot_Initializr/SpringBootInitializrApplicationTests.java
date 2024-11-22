c6fbe9e617a5b4e814dbd28239dae27732a0928e
Varad Mule <varadmule17@gmail.com>
2024-11-22 12:54:24 +0530

    Add files via upload

---
+++ SpringBoot_Initializr/src/test/java/com/jbk/SpringBoot_Initializr/SpringBootInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbk.SpringBoot_Initializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
